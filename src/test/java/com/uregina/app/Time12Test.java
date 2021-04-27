0484a52575ffc2b7820a854c91ef4bda44ad2fe7
karleefidek <kdf131@uregina.ca>
2021-04-27 13:24:03 -0600

    Add time12 test

--- src/test/java/com/uregina/app/Time12Test.java
+++ src/test/java/com/uregina/app/Time12Test.java
@@ -21,38 +21,110 @@ public class Time12Test
     }
 
     @Test
-    public void subtract_equalTimes_0pass()
+    public void subtract_10am10am_0pass()
     {
         try {
-            Time12 t1 = new Time12(1, 14, AmPm.pm);
-            Time12 t2 = new Time12(12, 0, AmPm.pm);
-            assertEquals( 800, time.subtract(t1, t2));
+            Time12 t1 = new Time12(10, 0, AmPm.am);
+            Time12 t2 = new Time12(10, 0, AmPm.am);
+            assertEquals( 0, time.subtract(t1, t2));
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+    }
+
+    @Test
+    public void subtract_10am115pm_neg195pass()
+    {
+        try {
+            Time12 t1 = new Time12(10, 0, AmPm.am);
+            Time12 t2 = new Time12(1, 15, AmPm.pm);
+            assertEquals( -195, time.subtract(t1, t2));
         } catch (Exception e) {
             //TODO: handle exception
         }
     }
 
     // @Test
-    // public void toTime24_245pm_1445pass()
+    // public void subtract_10am10am_0pass()
     // {
-    //     Time24 convertedTime = time.toTime24(2, 45, AmPm.pm);
-    //     assertEquals( 14, convertedTime.getHours());
-    //     assertEquals( 45, convertedTime.getMinutes());
+    //     try {
+    //         Time12 t1 = new Time12(10, 0, AmPm.am);
+    //         Time12 t2 = new Time12(10, 0, AmPm.am);
+    //         assertEquals( 0, time.subtract(t1, t2));
+    //     } catch (Exception e) {
+    //         //TODO: handle exception
+    //     }
+    // }
+
+    @Test
+    public void subtract_115pm115pm_0pass()
+    {
+        try {
+            Time12 t1 = new Time12(1, 15, AmPm.pm);
+            Time12 t2 = new Time12(1, 15, AmPm.pm);
+            assertEquals( 0, time.subtract(t1, t2));
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+    }
+
+    // @Test
+    // public void subtract_10am10am_0pass()
+    // {
+    //     try {
+    //         Time12 t1 = new Time12(10, 0, AmPm.am);
+    //         Time12 t2 = new Time12(10, 0, AmPm.am);
+    //         assertEquals( 0, time.subtract(t1, t2));
+    //     } catch (Exception e) {
+    //         //TODO: handle exception
+    //     }
+    // }
+
+    @Test
+    public void subtract_115pm10am_195pass()
+    {
+        try {
+            Time12 t1 = new Time12(1, 15, AmPm.pm);
+            Time12 t2 = new Time12(10, 0, AmPm.am);
+            assertEquals( 195, time.subtract(t1, t2));
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+    }
+    
+    // @Test
+    // public void subtract_115pm10am_195pass()
+    // {
+    //     try {
+    //         Time12 t1 = new Time12(1, 15, AmPm.pm);
+    //         Time12 t2 = new Time12(10, 0, AmPm.am);
+    //         assertEquals( 195, time.subtract(t1, t2));
+    //     } catch (Exception e) {
+    //         //TODO: handle exception
+    //     }
     // }
 
     // @Test
-    // public void toTime24_1045am_1045pass()
+    // public void subtract_115pm10am_195pass()
     // {
-    //     Time24 convertedTime = time.toTime24(10, 45, AmPm.am);
-    //     assertEquals( 10, convertedTime.getHours());
-    //     assertEquals( 45, convertedTime.getMinutes());
+    //     try {
+    //         Time12 t1 = new Time12(1, 15, AmPm.pm);
+    //         Time12 t2 = new Time12(10, 0, AmPm.am);
+    //         assertEquals( 195, time.subtract(t1, t2));
+    //     } catch (Exception e) {
+    //         //TODO: handle exception
+    //     }
     // }
 
     // @Test
-    // public void toTime24_1245pm_1245pass()
+    // public void subtract_115pm10am_195pass()
     // {
-    //     Time24 convertedTime = time.toTime24(12, 45, AmPm.pm);
-    //     assertEquals( 12, convertedTime.getHours());
-    //     assertEquals( 45, convertedTime.getMinutes());
+    //     try {
+    //         Time12 t1 = new Time12(1, 15, AmPm.pm);
+    //         Time12 t2 = new Time12(10, 0, AmPm.am);
+    //         assertEquals( 195, time.subtract(t1, t2));
+    //     } catch (Exception e) {
+    //         //TODO: handle exception
+    //     }
     // }
 }
