61f282c360db15b94842aa2e8a94216d311f3eae
karleefidek <kdf131@uregina.ca>
2021-04-27 12:01:09 -0600

    fix test

--- src/test/java/com/uregina/app/Time12Test.java
+++ src/test/java/com/uregina/app/Time12Test.java
@@ -27,12 +27,10 @@ public class Time12Test
         try {
             Time12 t1 = new Time12(12, 0, AmPm.am);
             Time12 t2 = new Time12(12, 0, AmPm.am);
+            assertEquals( 0, time.subtract(t1, t2));
         } catch (Exception e) {
             //TODO: handle exception
         }
-        
-        assertEquals( 0, time.subtract(t1, t2));
-        assertEquals( 45, convertedTime.getMinutes());
     }
 
     // @Test
