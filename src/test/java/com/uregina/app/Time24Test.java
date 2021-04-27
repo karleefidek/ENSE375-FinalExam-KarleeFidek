4ac872a2e3ac6d72d0aa7c27ab2fcbd6ed8387c0
karleefidek <kdf131@uregina.ca>
2021-04-27 11:01:53 -0600

    Add time24 tests

--- src/test/java/com/uregina/app/Time24Test.java
+++ src/test/java/com/uregina/app/Time24Test.java
@@ -1,6 +1,7 @@
 package com.uregina.app;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -9,12 +10,36 @@ import org.junit.Test;
  */
 public class Time24Test 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
-    public void shouldAnswerWithTrue()
+    public void toTime24_1245am_045pass()
     {
-        assertTrue( false );
+        Time24 time = toTime24(12, 45, AmPm.am);
+        assertEquals( 0, time.getHours());
+        assertEquals( 45, time.getMinutes());
+    }
+
+    @Test
+    public void toTime24_245pm_1445pass()
+    {
+        Time24 time = toTime24(2, 45, AmPm.pm);
+        assertEquals( 14, time.getHours());
+        assertEquals( 45, time.getMinutes());
+    }
+
+    @Test
+    public void toTime24_1045am_1045pass()
+    {
+        Time24 time = toTime24(10, 45, AmPm.am);
+        assertEquals( 10, time.getHours());
+        assertEquals( 45, time.getMinutes());
+    }
+
+    @Test
+    public void toTime24_1245pm_1245pass()
+    {
+        Time24 time = toTime24(12, 45, AmPm.pm);
+        assertEquals( 12, time.getHours());
+        assertEquals( 45, time.getMinutes());
     }
 }
