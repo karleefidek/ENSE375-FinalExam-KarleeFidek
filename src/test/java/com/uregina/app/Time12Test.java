d634cf0dbb0d0aa2ed854ac39edb5c39aba64d60
karleefidek <kdf131@uregina.ca>
2021-04-27 13:41:19 -0600

    update tests

--- src/test/java/com/uregina/app/Time12Test.java
+++ src/test/java/com/uregina/app/Time12Test.java
@@ -45,18 +45,6 @@ public class Time12Test
     }
 
     @Test
-    public void subtract_10am13am_invalidpass()
-    {
-        try {
-            Time12 t1 = new Time12(10, 0, AmPm.am);
-            Time12 t2 = new Time12(13, 0, AmPm.am);
-        } catch (Exception e) {
-            //TODO: handle exception
-            assertEquals(InvalidTimeException.class, e.class);
-        }
-    }
-
-    @Test
     public void subtract_115pm115pm_0pass()
     {
         try {
@@ -69,18 +57,6 @@ public class Time12Test
     }
 
     @Test
-    public void subtract_115pm13am_invalidpass()
-    {
-        try {
-            Time12 t1 = new Time12(1, 15, AmPm.pm);
-            Time12 t2 = new Time12(13, 0, AmPm.am);
-        } catch (Exception e) {
-            //TODO: handle exception
-            assertEquals(InvalidTimeException.class, e.class);
-        }
-    }
-
-    @Test
     public void subtract_115pm10am_195pass()
     {
         try {
@@ -89,42 +65,6 @@ public class Time12Test
             assertEquals( 195, time.subtract(t1, t2));
         } catch (Exception e) {
             //TODO: handle exception
-        }
-    }
-    
-    @Test
-    public void subtract_13pm13pm_invalidpass()
-    {
-        try {
-            Time12 t1 = new Time12(13, 0, AmPm.pm);
-            Time12 t2 = new Time12(13, 0, AmPm.pm);
-        } catch (Exception e) {
-            //TODO: handle exception
-            assertEquals(InvalidTimeException.class, e.class);
-        }
-    }
-
-    @Test
-    public void subtract_13pm10am_invalidpass()
-    {
-        try {
-            Time12 t1 = new Time12(13, 0, AmPm.pm);
-            Time12 t2 = new Time12(10, 0, AmPm.am);
-        } catch (Exception e) {
-            //TODO: handle exception
-            assertEquals(InvalidTimeException.class, e.class);
-        }
-    }
-
-    @Test
-    public void subtract_13pm115pm_invalidpass()
-    {
-        try {
-            Time12 t1 = new Time12(13, 0, AmPm.pm);
-            Time12 t2 = new Time12(1, 15, AmPm.pm);
-        } catch (Exception e) {
-            //TODO: handle exception
-            assertEquals(InvalidTimeException.class, e.class);
         }
     }
 }
