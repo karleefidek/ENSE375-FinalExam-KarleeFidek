bd3e7012e29ab095d03df87161577547fac7e2de
karleefidek <kdf131@uregina.ca>
2021-04-27 19:00:00 -0600

    add tests

--- src/test/java/com/uregina/app/TicketTest.java
+++ src/test/java/com/uregina/app/TicketTest.java
@@ -15,7 +15,7 @@ public class TicketTest
 
     //checkTicket( ArrayList<Flight> ticket, int maxFlightsCount, int maxFlightTime, int maxLayoverTime, boolean hasSchengenVisa)
     @Test
-    public void subtract_date1IsDayAfterDate2_1440()
+    public void checkTicket_maxflights_false()
     {
         ArrayList<Flight> ticket = new ArrayList<Flight>();
         try {
@@ -29,6 +29,40 @@ public class TicketTest
             //TODO: handle exception
         }
         assertFalse(Ticket.checkTicket(ticket, 1, 9000, 9000, true));
+    }
+
+    @Test
+    public void checkTicket_IncorrectAirportCode_false()
+    {
+        ArrayList<Flight> ticket = new ArrayList<Flight>();
+        try {
+            ticket.add(new Flight("YY", "KL", 
+                new DateTime(new Date(12, 20, 2000), new Time12(1, 0, AmPm.pm)), 
+                new DateTime(new Date(12, 21, 2000), new Time12(11, 0, AmPm.pm))));
+            ticket.add(new Flight("KL", "WEE", 
+                new DateTime(new Date(12, 21, 2000), new Time12(11, 30, AmPm.pm)), 
+                new DateTime(new Date(12, 22, 2000), new Time12(12, 0, AmPm.pm))));
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+        assertFalse(Ticket.checkTicket(ticket, 4, 9000, 9000, true));
+    }
+
+    @Test
+    public void checkTicket_2SchengenAirports_false()
+    {
+        ArrayList<Flight> ticket = new ArrayList<Flight>();
+        try {
+            ticket.add(new Flight("BRU", "VIE", 
+                new DateTime(new Date(12, 20, 2000), new Time12(1, 0, AmPm.pm)), 
+                new DateTime(new Date(12, 21, 2000), new Time12(11, 0, AmPm.pm))));
+            ticket.add(new Flight("VIE", "WEE", 
+                new DateTime(new Date(12, 21, 2000), new Time12(11, 30, AmPm.pm)), 
+                new DateTime(new Date(12, 22, 2000), new Time12(12, 0, AmPm.pm))));
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+        assertFalse(Ticket.checkTicket(ticket, 4, 9000, 9000, true));
     }
 
 }
