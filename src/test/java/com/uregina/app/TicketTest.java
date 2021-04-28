09b0271fc9e5a6a48eb5a59f2c19637cd77ec916
karleefidek <kdf131@uregina.ca>
2021-04-27 18:51:50 -0600

    update

--- src/test/java/com/uregina/app/TicketTest.java
+++ src/test/java/com/uregina/app/TicketTest.java
@@ -18,12 +18,16 @@ public class TicketTest
     public void subtract_date1IsDayAfterDate2_1440()
     {
         ArrayList<Flight> ticket = new ArrayList<Flight>();
-        ticket.add(new Flight("YYY", "KLL", 
-            new DateTime(new Date(12, 20, 2000), new Time12(1, 0, AmPm.pm)), 
-            new DateTime(new Date(12, 21, 2000), new Time12(11, 0, AmPm.pm))));
-        ticket.add(new Flight("KLL", "WEE", 
-            new DateTime(new Date(12, 21, 2000), new Time12(11, 30, AmPm.pm)), 
-            new DateTime(new Date(12, 22, 2000), new Time12(12, 0, AmPm.pm))));
+        try {
+            ticket.add(new Flight("YYY", "KLL", 
+                new DateTime(new Date(12, 20, 2000), new Time12(1, 0, AmPm.pm)), 
+                new DateTime(new Date(12, 21, 2000), new Time12(11, 0, AmPm.pm))));
+            ticket.add(new Flight("KLL", "WEE", 
+                new DateTime(new Date(12, 21, 2000), new Time12(11, 30, AmPm.pm)), 
+                new DateTime(new Date(12, 22, 2000), new Time12(12, 0, AmPm.pm))));
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
         assertFalse(Ticket.checkTicket(ticket, 1, 9000, 9000, true));
     }
 
